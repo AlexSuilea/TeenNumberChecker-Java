7dad532f03211498b64648b80ff124952a18b2f2
Alexandru Suilea <42109259+AlexSuilea@users.noreply.github.com>
2020-07-08 01:03:27 +0300

    Add files via upload

---
+++ TeenNumberChecker/src/com/company/TeenNumberChecker.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class TeenNumberChecker {
+    public static boolean hasTeen(int number1, int number2, int number3){
+        return (number1 >= 13 && number1 <= 19) || (number2 >= 13 && number2 <= 19) || (number3 >= 13 && number3 <= 19);
+    }
+
+    public static boolean isTeen(int number){
+        return number >= 13 && number <= 19;
+    }
+}
